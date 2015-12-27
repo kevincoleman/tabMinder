566a05316218892095bf939b8337b25892885c1a
kevincoleman <kevin.n.coleman@gmail.com>
2015-12-26 15:09:01 -1000

    initial commit

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
+// npm includes
+var fs = require('fs');
 var express = require('express');
 var routes = require('./routes');
- 
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+var assert = require('assert');
+
+// local includes
+var Db = require('./lib/db.js');
+var dataModel = JSON.parse(fs.readFileSync('./data/model.json', 'utf-8'));
+
+// var declarations
+var url = 'mongodb://localhost:27017/tabMinder';
+
+// app setup
 var app = express();
 var server = app.listen(3000);
 var io = require('socket.io').listen(server);
 
-  // configure express app
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.static(__dirname + '/public'));
+// configure express app
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
  
 app.get('/', require('./routes/index'));
 
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   Db.insertDocument(db, dataModel.patron, function(){
+//     Db.retrieveDocument(db, ObjectId("567f1ddeeb26625bbccd9f58"), function() {
+//       db.close();
+//     });
+//   });
+// });
+
 io.on('connection', function(socket){
   console.log('a client has connected');
-  io.emit('hi');
-})
+});
  
 console.log("Express server listening on port 3000");
